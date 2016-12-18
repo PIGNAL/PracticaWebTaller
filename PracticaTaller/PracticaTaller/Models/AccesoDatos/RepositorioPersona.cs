320d93411d4146a3f7b1678e1d9e26f0f898e64f
PIGNAL <joni_ballatore@hotmail.com>
2016-12-17 21:55:09 -0300

    subida de programa actualizado

---
+++ PracticaTaller/PracticaTaller/Models/AccesoDatos/RepositorioPersona.cs
@@ -0,0 +1,18 @@
+﻿using PracticaTaller.Models.AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticaTaller.Models
+{
+    public class RepositorioPersona
+    {
+        PracticaJonathanDB taller = new PracticaJonathanDB();
+        public void Guardar(Persona persona)
+        {
+            taller.Persona.Add(persona);
+            taller.SaveChanges();
+        }
+    }
+}
---
+++ PracticaTaller/PracticaTaller/Models/LogicaNegocio/GestorPersona.cs
@@ -0,0 +1,17 @@
+﻿using PracticaTaller.Models.AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticaTaller.Models.LogicaNegocio
+{
+    public class GestorPersona
+    {
+        RepositorioPersona repo = new RepositorioPersona();
+        public void Guardar(Persona persona)
+        {
+            repo.Guardar(persona);
+        }
+    }
+}
